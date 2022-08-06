87a890406f3e80d5becd34c3ba641abe137df4f4
return764 <543851436@qq.com>
2022-08-06 17:02:25 +0800

    chore: build basic test construction

--- src/test/java/com/cn/yutao/together_backend/TogetherBackendApplicationTests.java
+++ src/test/java/com/cn/yutao/together_backend/TogetherBackendApplicationTests.java
@@ -1,13 +1,24 @@
 package com.cn.yutao.together_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TogetherBackendApplicationTests {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    void should_hello_world() {
+        final var responseEntity = restTemplate.getForEntity("/hello", String.class);
+
+        assertThat(responseEntity.getBody()).isEqualTo("World");
     }
 
 }

ff19881f31db2e8c77f3fa1ab8adeaad02103448
return764 <543851436@qq.com>
2022-08-06 16:31:56 +0800

    chore: build project

---
+++ src/main/java/com/cn/yutao/together_backend/TogetherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cn.yutao.together_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cn/yutao/together_backend/TogetherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.yutao.together_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

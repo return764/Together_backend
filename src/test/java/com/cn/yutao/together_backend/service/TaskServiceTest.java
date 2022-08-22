be92914c100b29428114e2855c86ab763ad0b64b
return764 <543851436@qq.com>
2022-08-22 18:34:54 +0800

    feat: add test for TaskServiceTest

--- src/test/java/com/cn/yutao/together_backend/service/TaskServiceTest.java
+++ src/test/java/com/cn/yutao/together_backend/service/TaskServiceTest.java
@@ -1,6 +1,7 @@
 package com.cn.yutao.together_backend.service;
 
 import com.cn.yutao.together_backend.entity.Task;
+import com.cn.yutao.together_backend.exception.TaskNotFoundException;
 import com.cn.yutao.together_backend.repository.TaskRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,12 +9,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,34 +27,33 @@ class TaskServiceTest {
     @InjectMocks
     TaskService taskService;
     private List<Task> tasks;
+    private Task task;
 
     @BeforeEach
     void setUp() {
-        tasks = List.of(new Task("test", "test", 0),
+        task = new Task("test", "test", 0);
+        tasks = List.of(task,
                 new Task("test", "test", 1),
                 new Task("test", "test", 2));
     }
 
-//    @Test
-//    void should_return_all_tasks_when_given_null_status() {
-//        // given
-//        when(repository.findAll()).thenReturn(tasks);
-//        // when
-//        final var result = taskService.fetchTasks(null);
-//        // then
-//        assertThat(result).hasSize(3).isEqualTo(tasks);
-//        verify(repository, times(1)).findAll();
-//    }
-//
-//    @Test
-//    void should_return_tasks_filter_by_status() {
-//        // given
-//        when(repository.findAll(any(Specification.class)))
-//                .thenReturn(List.of(tasks.get(1)));
-//        // when
-//        final var result = taskService.fetchTasks(1);
-//        // then
-//        assertThat(result).hasSize(1).isEqualTo(List.of(tasks.get(1)));
-//        verify(repository, times(1)).findAll(any(Specification.class));
-//    }
+    @Test
+    void should_return_task_fetch_by_id() {
+        // given
+        when(repository.findById(1L)).thenReturn(Optional.of(task));
+        // when
+        final var result = taskService.fetchTaskById(1L);
+        // then
+        assertThat(result).isEqualTo(task);
+        verify(repository, times(1)).findById(1L);
+    }
+
+    @Test
+    void should_throw_task_not_found_exception_when_fetch_null_task() {
+        // given
+        when(repository.findById(1L)).thenReturn(Optional.empty());
+        // when
+        // then
+        assertThrows(TaskNotFoundException.class, () -> taskService.fetchTaskById(1L));
+    }
 }
